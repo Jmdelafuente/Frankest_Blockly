8aa71bcee4a2fe67d3545b8ed0aaab7f5d4eecd4
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2021-06-15 20:30:33 -0300

    NEW FEAT - save workspace in server - restore workspace from server - run blocks (via Js interpretation) - highligh blocks on run - console output - modals into blockly (add-on) - spinner + overlay animation
    FIX
    - change robot selection
    - dynamically load toolbox, lang and block definition from server
    - prettyprint in case of use
    - responsive workspace
    - media files

---
+++ code/js/utils.js
@@ -0,0 +1,46 @@
+var consoleLine = 0;
+
+function showComponent(componentName,arrowName) {
+  var i;
+  var x = document.getElementsByClassName("component");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(componentName + "-tr").classList.toggle("is-active");
+  if(document.getElementById(componentName).parentElement.style.display == "block"){
+    document.getElementById(componentName).parentElement.style.display = "none";
+    document.getElementById(arrowName).classList = "arrow up";
+    document.getElementById("content_blocks").style.height = "88%";
+  }else{
+    document.getElementById(componentName).parentElement.style.display =
+      "block";
+    document.getElementById(arrowName).classList = "arrow down";
+    document.getElementById("content_blocks").style.height = "75%";
+  }
+}
+
+function addConsoleMessage(message){
+  let IOconsole = document.getElementById("console");
+  let p = document.createElement("p");
+  p.id = "cl-"+consoleLine;
+  p.innerText = message;
+  if(IOconsole.parentElement.style.display == "none"){
+    IOconsole.parentElement.style.display = "block";
+  }
+  IOconsole.append(p);
+  IOconsole.scrollTop = IOconsole.scrollHeight - IOconsole.clientHeight;
+}
+
+function showModal(message){
+  const modal = new Modal(message,Code.workspace);
+  modal.init();
+  modal.show();
+}
+
+ function overlayOn() {
+   document.getElementById("overlay").style.display = "block";
+ }
+
+ function overlayOff() {
+   document.getElementById("overlay").style.display = "none";
+ }
